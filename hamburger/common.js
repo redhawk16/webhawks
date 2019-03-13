eaaa859057955eeaff127a10ba0708862cad01ba
redhawk16 <thomasgameone@gmail.com>
2019-03-13 21:39:28 +0600

    Update

---
+++ hamburger/common.js
@@ -0,0 +1,5 @@
+function hamburger() {
+	var hamburger = document.getElementById('hamburger-menu');
+	hamburger.classList.toggle('hamburger-menu--hide');
+	hamburger.classList.toggle('hamburger-menu--show');
+}
---
+++ site/dist/js/common.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+	particlesJS.load('particles-js', '../assets/etc/particlesjs-config.json', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+});
